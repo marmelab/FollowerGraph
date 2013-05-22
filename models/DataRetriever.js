b6681691f3a86a89c734b7bf9c245f638e060bca
Emmanuel Quentin <manu.quentin@gmail.com>
2013-05-22 18:31:06 +0200

    Add data retrieving cache Change graph links

--- models/DataRetriever.js
+++ models/DataRetriever.js
@@ -50,6 +50,7 @@ DataRetriever.prototype.retrieveAndStoreData = function(idUser, cb){
 		function(oAllIds, next){
 			// Retrieve data of the followers & followings of the current user
 			self.retrieveDataFor(Object.keys(oAllIds), function(err, res){
+				self.saveData('data', res, false);
 				if(err){
 					return cb(err);
 				}
@@ -70,7 +71,7 @@ DataRetriever.prototype.retrieveAndStoreData = function(idUser, cb){
 					var idToRetrieve = aIdsToRetrieve[current];
 
 					// If the current user has more than 15000 followers or followings, skip
-					if(res[idToRetrieve].followers_count > 15000 || res[idToRetrieve].friends_count > 1500){
+					if(res[idToRetrieve].followers_count > 15000 || res[idToRetrieve].friends_count > 15000){
 						console.log('Skip : '+res[idToRetrieve].screen_name+' (followers : '+res[idToRetrieve].followers_count+')');
 
 						current++;
@@ -104,26 +105,40 @@ DataRetriever.prototype.retrieveAndStoreData = function(idUser, cb){
 		function (oAllIds, oOtherIds, next){
 			// Retrieve others data
 			self.retrieveDataFor(Object.keys(oOtherIds), function(err, otherRslt){
+				// Store users data enven if we've got an error
+				self.saveData('data', otherRslt, false);
+
 				if(err){
 					return cb(err);
 				}
 
+				// Merge with previously retrieved users data
 				for(var z in otherRslt){
-					oAllIds[z] = otherRslt[z];
+					if(otherRslt.hasOwnProperty(z)){
+						oAllIds[z] = otherRslt[z];
+					}
 				}
 
 				next(null, oAllIds);
 			});
-		},
-
-		function (oAllIds, next){
-			self.saveData('data', oAllIds, next);
 		}
 	], cb);
 };
 
+/**
+ * Retrieve followers & followings for an user and store theses data in a file
+ * @param Number idUser
+ * @param Function cb
+ */
 DataRetriever.prototype.retrieveAndStoreLinksFor = function(idUser, cb){
 	console.log('Retrieving followers/followings for :'+idUser);
+
+	// Check if we already got the data
+	var data = this.getFileData(idUser+'.json');
+	if(data && (data.followers.length || data.followers.length)){
+		console.log('Cached');
+		return cb(null, data);
+	}
 
 	var self = this;
 
@@ -142,12 +157,57 @@ DataRetriever.prototype.retrieveAndStoreLinksFor = function(idUser, cb){
 		self.saveData(idUser, oData);
 		cb(null, oData);
 	});
-}
+};
 
-DataRetriever.prototype.saveData = function(fileName, data){
-	fs.writeFileSync(dataDir+fileName+'.json', JSON.stringify(data));
-}
+/**
+ * Store JSON data in a file
+ * @param String fileName
+ * @param Object data
+ * @param Boolean override
+ */
+DataRetriever.prototype.saveData = function(fileName, data, override){
+	override = override == undefined ? true : override;
+	var filename = fileName+'.json';
 
+	// Shall we override the file ?
+	if(!override){
+		var oldData = this.getFileData(filename);
+
+		if(oldData){
+			// Merge with old data
+			for(var z in data){
+				if(data.hasOwnProperty(z)){
+					oldData[z] = data[z];
+				}
+			}
+
+			data = oldData;
+		}
+	}
+
+	fs.writeFileSync(dataDir+filename, JSON.stringify(data));
+};
+
+/**
+ * Retrieve JSON Object data from a file
+ * @param String file
+ * @returns Object
+ */
+DataRetriever.prototype.getFileData = function(file){
+	file = dataDir+file;
+
+	if(fs.existsSync(file)){
+		return JSON.parse(fs.readFileSync(file, 'utf8'));
+	}
+
+	return null;
+};
+
+/**
+ * Returns followers & followings from an user
+ * @param Number idUser
+ * @param Function cb
+ */
 DataRetriever.prototype.retrieveLinksFor = function(idUser, cb){
 	var self = this;
 
@@ -177,19 +237,37 @@ DataRetriever.prototype.retrieveLinksFor = function(idUser, cb){
 	], cb);
 };
 
+/**
+ * Retrieve data for multiple users
+ * @param Array aUserIds
+ * @param Function cb
+ */
 DataRetriever.prototype.retrieveDataFor = function(aUserIds, cb){
+	// Get already retrieved data
+	var usersData     = {};
+	var oldData       = this.getFileData('data.json');
+	var aIdsToExclude = [];
+	if(oldData){
+		usersData         = oldData;
+		var aIdsToExclude = Object.keys(oldData);
+	}
+
+	// Filter users for whom we've already got data
+	aUserIds = aUserIds.filter(function(i){
+		return !(aIdsToExclude.indexOf(i) >= 0);
+	});
+
 	var nbIds         = aUserIds.length;
 	var currentPage   = 1;
 	var itemsPerCall  = 100;
-	var usersData     = {};
 	var self          = this;
 
+	// No user to retrieve ? return the result directly
 	if(nbIds == 0){
-		return cb(null, []);
+		return cb(null, usersData);
 	}
 
-	console.log('Retrieve users data');
-	console.log('Nb to retrieve : '+nbIds);
+	console.log('Retrieve users data : '+nbIds);
 	(function usersLookup(){
 		var currentPos = (currentPage - 1) * itemsPerCall;
 		console.log('Pos : '+currentPos);
@@ -202,34 +280,49 @@ DataRetriever.prototype.retrieveDataFor = function(aUserIds, cb){
 
 		self.client.get('users/lookup', {user_id: sIds}, function(err, datas){
 			if(err){
-				return cb(err);
+				return cb(err, usersData);
 			}
 
+			// Merge user data
 			for(var i = 0, length = datas.length; i < length; i++){
 				var data = datas[i];
 
 				usersData[data.id_str] = data;
 			}
 
+			// Next iteration
 			currentPage++;
 			usersLookup();
 		});
 	})();
-}
+};
 
+/**
+ * Call a cursored Twitter API resource
+ * @param String method
+ * @param String url
+ * @param Object params
+ * @param Function cb
+ */
 DataRetriever.prototype.callWithPagination = function(method, url, params, cb){
 	var self      = this;
 	var results   = [];
 
-
+	// Call the API resource recursively
 	(function processCall(method, url, params, cursor){
 		params.cursor = cursor || -1;
 
 		console.log('Call :'+url+' ('+JSON.stringify(params)+')');
 		self.client[method](url, params, function(err, res){
 			if(err){
-				//return cb(err);
+				// 401 : Private account
+				if(err.statusCode == 401){
+					return cb(null, {});
+				}else{
+					return cb(err);
+				}
 			}else{
+				// Merge with data retrieved in previous call
 				results = results.concat(res.ids);
 			}
 
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,14 +1,17 @@
-var width               = 900;
-var height              = 600;
+var width               = window.innerWidth;
+var height              = window.innerHeight;
 var biDirectionalForce  = 10;
 var followingForce      = 8;
 var followedForce       = 4;
 var colors              = d3.scale.category20();
-var currentUserId       = 1153172437;
+var currentUserId       = 14089153;
+var nodes               = [];
+var links               = [];
+var currentCard         = null;
 
 var force = d3.layout.force()
 		.charge(-120)
-		.linkDistance(30)
+		.distance(300)
 		.size([width, height]);
 
 // Create SVG
@@ -22,6 +25,7 @@ d3.json('/nodes', function(err, files){
 		return console.error(err);
 	}
 
+	// Raw data of the display node
 	var rawNodes = {};
 	var nbFiles  = files.length;
 	var current  = 0;
@@ -59,24 +63,31 @@ d3.json('/nodes', function(err, files){
 
 function displayGraph(rawNodes, data){
 	var aNodeMap  = {};
-	var nodes     = [];
-	var links     = [];
-
 
 	// Parse each nodes
 	for(var idUser in rawNodes){
 		// Create node
 		nodes.push({
-			name: data[idUser].screen_name,
-			id: idUser,
-			group: idUser == currentUserId ? 1 : 2
+			name:       data[idUser] ? '@'+data[idUser].screen_name : 'Anonymous',
+			fullname:   data[idUser] ? data[idUser].name : '',
+			image :     data[idUser] ? data[idUser].profile_image_url_https : 'https://si0.twimg.com/sticky/default_profile_images/default_profile_4_bigger.png',
+			id:         idUser,
+			group:      idUser == currentUserId ? 1 : 2,
+			followers:  data[idUser] ? data[idUser].followers_count : 0,
+			followings: data[idUser] ? data[idUser].friends_count : 0
 		});
 
 		aNodeMap[idUser] = nodes.length - 1;
 	}
 
-	// Add links
+	// Add links between nodes
 	for(var idUser in rawNodes){
+
+		// Do not display current user links
+		if(idUser == currentUserId){
+			continue;
+		}
+
 		var oNode         = rawNodes[idUser];
 		var sourceIndex   = aNodeMap[idUser];
 
@@ -90,17 +101,32 @@ function displayGraph(rawNodes, data){
 			for(var j = 0, length = aLink.length; j < length; j++){
 				var idElement = aLink[j];
 
+				// Do not display current user links
+				if(idElement == currentUserId){
+					continue;
+				}
+
 				// Check if the link point to an existing node
 				var destIndex = aNodeMap[idElement];
 				if(!destIndex){
 					continue;
 				}
 
+				var isFollower = oNode.followers.indexOf(parseInt(idElement, 10)) > -1;
+				var isFollowing = rawNodes[idElement].followers.length ? rawNodes[idElement].followers.indexOf(parseInt(idUser, 10)) > -1 : false;
+
+				var val = followedForce;
+				if(isFollower && isFollowing){
+					val = biDirectionalForce;
+				}else if(isFollowing){
+					val = followingForce;
+				}
+
 				// Create links if we have source & destination node
 				links.push({
 					source: sourceIndex,
 					target: destIndex,
-					value: 1
+					value: val
 				});
 			}
 		}
@@ -116,30 +142,162 @@ function displayGraph(rawNodes, data){
 			.data(links)
 			.enter().append("line")
 			.attr("class", "link")
-			.style("stroke-width", function(d) { return (d.value / 2); });
+			.style("stroke-width", function(d) { return (d.value /100); })
+			.style("stroke-opacity", function(d) { return (d.value *10); });
 
 	var node = svg.selectAll(".node")
 			.data(nodes)
-			.enter().append("circle")
+			.enter().append("g")
 			.attr("class", "node")
-			.attr("r", 5)
-			.style("fill", function(d) { return colors(d.group); })
+			.attr('id', function(d){ return 'node-'+d.id});
+
+	node
+			.append("image")
+			.attr("xlink:href", function(d){ return d.image })
+			.attr('width', 48)
+			.attr('height', 48)
+			.attr('x', -24)
+			.attr('y', -24)
+			.attr('style', 'clip-path: url(#clipCircle);')
+			.attr('transform', 'scale(0.6 0.6)')
 			.call(force.drag);
 
-	node.append("title")
-			.text(function(d) { return d.name; });
-
-	node.on('click', function(e){
-		console.log(e.id+':'+e.name);
+	node.on('click', function(e, f){
+		displayCard(e);
 	});
 
 	force.on("tick", function() {
+		// Move graph on tick
 		link.attr("x1", function(d) { return d.source.x; })
 				.attr("y1", function(d) { return d.source.y; })
 				.attr("x2", function(d) { return d.target.x; })
 				.attr("y2", function(d) { return d.target.y; });
 
-		node.attr("cx", function(d) { return d.x; })
-				.attr("cy", function(d) { return d.y; });
+		node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+		if(currentCard){
+			// Also move card if needed
+			currentCard.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")" });
+		}
 	});
+
+	svg.on('click', function(){
+		if(!currentCard){
+			return;
+		}
+
+		// Retrieve current card dimension & position
+		var rect = currentCard.select('rect')[0][0];
+		var pos = d3.mouse(rect);
+
+		// Have we clicked outside the current card ?
+		if(pos[0] < -24 || pos[1] < -24 || pos[0] > rect.attributes.width.value || pos[1] > rect.attributes.height.value){
+			removeCard();
+		}
+	});
+}
+
+/**
+ * Hide current card
+ */
+function removeCard(){
+	if(currentCard){
+		// Remove image
+		currentCard.select('image')
+				.transition()
+				.attr('style', 'clip-path: url(#clipCircle);')
+				.attr('transform', 'scale(0.6 0.6)')
+				.ease("out")
+				.duration(1000)
+				.each("end", function() {
+					this.remove();
+				});
+
+		// Remove texts
+		currentCard.selectAll('text, rect')
+				.attr('style', 'opacity: 1')
+				.transition()
+				.attr('style', 'opacity: 0')
+				.ease("out")
+				.duration(1000)
+				.each("end", function() {
+					this.remove();
+				});
+
+		currentCard = null;
+	}
+}
+
+/**
+ * Display a twitted card for the clicked node
+ * @param Object element
+ */
+function displayCard(element){
+	// Remove previous card
+	removeCard();
+
+	var idUser      = element.id;
+	var currentNode = d3.select('#node-'+idUser);
+	if(!currentNode.length){
+		return;
+	}
+
+	// Add a group which will contains the card
+	currentCard = svg
+			.data([element])
+			.append('g')
+			.attr('class', 'card')
+			.attr("transform", "translate(" + element.x + "," + element.y + ")");
+
+	// Image animation
+	currentCard
+			.append("image")
+			.attr("xlink:href", element.image)
+			.attr('width', 48)
+			.attr('height', 48)
+			.attr('x', -24)
+			.attr('y', -24)
+			.attr('style', 'clip-path: url(#clipCircle);')
+			.attr('transform', 'scale(0.6 0.6)')
+			.transition()
+			.ease("elastic")
+			.duration(1000)
+			.attr('style', 'clip-path: none')
+			.attr('transform', 'scale(1 1)');
+
+	var bg = currentCard
+			.append("rect")
+			.attr('fill', '#fff');
+
+	// Add centered text
+	var texts = [element.name, element.fullname, element.followers+' abonnés, '+ element.followings+ ' abonnements'];
+	var yText = 30;
+	var maxWidth = 0;
+	for(var i = 0, length = texts.length; i < length; i++){
+		var text = texts[i];
+
+		// Add Text
+		var textElt = currentCard
+				.append('text')
+				.text(text);
+
+		// Retrieve text dimensions
+		var textWidth   = textElt[0][0].offsetWidth;
+		var textHeight  = textElt[0][0].offsetHeight
+		maxWidth        = Math.max(maxWidth, textWidth);
+
+		textElt
+				.attr('x', -textWidth/2)
+				.attr('y', yText + textHeight);
+
+		yText += textHeight;
+	}
+
+	maxWidth += 20;
+
+	// Change the background dimensions depending of the texts
+	bg.attr('x', -maxWidth / 2)
+			.attr('y', 25)
+			.attr('width', maxWidth)
+			.attr('height', yText)
 }
